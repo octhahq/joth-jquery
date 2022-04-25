fc0c07e9ad46d9859846c2597be77fb579f94fc6
Lucas Nicolau <lucas@octha.com>
2022-04-24 22:36:14 -0300

    🎨 improving request data parse

--- joth.js
+++ joth.js
@@ -16,14 +16,14 @@ joth = (function (window, document, jquery) {
 				encode(str, secret) {
 					secret = joth.cipher.aes.getKeyAndIv(secret);
 
-					return joth.cipher.encode(window.CryptoJS.AES.encrypt(
-						str,
+					return window.CryptoJS.AES.encrypt(
+						joth.cipher.encode(str),
 						window.CryptoJS.enc.Utf8.parse(secret.key),
 						{
 							iv: window.CryptoJS.enc.Utf8.parse(secret.iv),
 							mode: window.CryptoJS.mode.CBC,
 						}
-					).toString())
+					).toString()
 				},
 
 				decode(str, secret) {
@@ -73,23 +73,9 @@ joth = (function (window, document, jquery) {
 			if (
 				this.modifyMethods.includes(options.type.toLowerCase())
 				&& request.data !== null
-				&& typeof request.data === 'object'
 			) {
-				var data = request.data,
+				var data = this.getParamsAsObject(typeof request.data === 'object' ? jquery.param(request.data) : request.data),
 					attrs = this.getAttrs(options.url);
-
-				if (
-					Array.isArray(data)
-					&& data.length > 0
-					&& typeof data[0] === 'object'
-					&& 'name' in data[0]
-					&& 'value' in data[0]
-				) {
-					data = data.reduce(function (accumulator, value) {
-						accumulator[value.name] = value.value;
-						return accumulator
-					}, {})
-				}
 
 				this.resolveAttrs(attrs, data, this.cipher.aes.encode);
 
@@ -159,6 +145,56 @@ joth = (function (window, document, jquery) {
 			}
 
 			return str.join('&');
+		},
+
+		getParamsAsObject(query) {
+			query = query.substring(query.indexOf('?') + 1);
+
+			var re = /([^&=]+)=?([^&]*)/g;
+			var decodeRE = /\+/g;
+
+			var decode = function (str) {
+				return decodeURIComponent(str.replace(decodeRE, " "));
+			};
+
+			var params = {}, e;
+			while (e = re.exec(query)) {
+				var k = decode(e[1]), v = decode(e[2]);
+
+				if (k.substring(k.length - 2) === '[]') {
+					k = k.substring(0, k.length - 2);
+					(params[k] || (params[k] = [])).push(v);
+				}
+				else params[k] = v;
+			}
+
+			var assign = function (obj, keyPath, value) {
+				var lastKeyIndex = keyPath.length - 1;
+				for (var i = 0; i < lastKeyIndex; ++i) {
+					var key = keyPath[i];
+					if (!(key in obj))
+						obj[key] = {};
+					obj = obj[key];
+				}
+
+				obj[keyPath[lastKeyIndex]] = value;
+			};
+
+			for (var prop in params) {
+				var structure = prop.split('[');
+
+				if (structure.length > 1) {
+					var levels = [];
+					structure.forEach(function (item, i) {
+						var key = item.replace(/[?[\]\\ ]/g, '');
+						levels.push(key);
+					});
+					assign(params, levels, params[prop]);
+					delete (params[prop]);
+				}
+			}
+
+			return params;
 		},
 
 		filter(data) {
